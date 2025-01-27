fef237de80f1eb727f738897152bfbf83d185ffc
Santiago-Cordoba <santiagocordobaduenas@gmail.com>
2025-01-27 18:10:21 -0500

    Punto 1 hecho

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,6 +12,19 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
+        CountThread thread1 = new CountThread(0,99);
+        CountThread thread2 = new CountThread(99,199);
+        CountThread thread3 = new CountThread(200,299);
+
+        System.out.println("***********mostrando threads con start**************");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        System.out.println("**************mostrando threads con run**************");
+        thread1.run();
+        thread2.run();
+        thread3.run();
         
     }
     
